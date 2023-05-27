7ae958303da8b4f6358ea00f22068231bed2e662
namgigun <namgigun@gmail.com>
2023-05-27 16:36:13 +0900

    Program.cs 윈폼 다중 실행으로 인해, 코드 수정

--- 객체지향 프로그래밍 프로젝트/객체지향 프로그래밍 프로젝트/Program.cs
+++ 객체지향 프로그래밍 프로젝트/객체지향 프로그래밍 프로젝트/Program.cs
@@ -2,6 +2,7 @@ namespace 객체지향_프로그래밍_프로젝트
 {
     internal static class Program
     {
+        private const string MutexName = "MyUniqueMutexName";
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
@@ -10,8 +11,20 @@ namespace 객체지향_프로그래밍_프로젝트
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            using (Mutex mutex = new Mutex(true, MutexName, out bool createdNew))
+            {
+                if (createdNew)
+                {
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    Application.Run(new MainPage());
+                }
+                else
+                {
+                    // 이미 실행 중인 경우 처리할 로직
+                    // 예를 들어 기존 인스턴스로 포커스를 이동시킬 수 있습니다.
+                }
+            }
         }
     }
 }
